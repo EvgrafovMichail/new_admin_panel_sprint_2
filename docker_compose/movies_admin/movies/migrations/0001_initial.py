5008f485e43edbdad16c142ce2516dafed7d68f8
EvgrafovMichail <evgrafovmd@gmail.com>
2024-04-08 22:53:07 +0300

    Update movies_admin migrations

--- docker_compose/movies_admin/movies/migrations/0001_initial.py
+++ docker_compose/movies_admin/movies/migrations/0001_initial.py
@@ -18,16 +18,33 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name="Filmwork",
             fields=[
-                ("id", models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
-                ("created", models.DateTimeField(auto_now_add=True)),
-                ("modified", models.DateTimeField(auto_now=True)),
+                (
+                    "id",
+                    models.UUIDField(
+                        default=uuid.uuid4,
+                        editable=False,
+                        primary_key=True,
+                        serialize=False,
+                    ),
+                ),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
                 ("title", models.TextField(verbose_name="title")),
-                ("description", models.TextField(blank=True, verbose_name="description")),
-                ("creation_date", models.DateField(blank=True, verbose_name="creation_date")),
+                (
+                    "description",
+                    models.TextField(blank=True, verbose_name="description"),
+                ),
+                (
+                    "creation_date",
+                    models.DateField(
+                        blank=True, null=True, verbose_name="creation_date"
+                    ),
+                ),
                 (
                     "rating",
                     models.FloatField(
                         blank=True,
+                        null=True,
                         validators=[
                             django.core.validators.MinValueValidator(0),
                             django.core.validators.MaxValueValidator(100),
@@ -35,7 +52,19 @@ class Migration(migrations.Migration):
                         verbose_name="rating",
                     ),
                 ),
-                ("type", models.TextField(choices=[("movie", "Movie"), ("tv_show", "Tv Show")], verbose_name="type")),
+                (
+                    "type",
+                    models.TextField(
+                        choices=[("movie", "Movie"), ("tv_show", "Tv Show")],
+                        verbose_name="type",
+                    ),
+                ),
+                (
+                    "file_path",
+                    models.FileField(
+                        blank=True, null=True, upload_to="movies/", verbose_name="file"
+                    ),
+                ),
             ],
             options={
                 "verbose_name": "Кинопроизведение",
@@ -46,11 +75,25 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name="Genre",
             fields=[
-                ("id", models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
-                ("created", models.DateTimeField(auto_now_add=True)),
-                ("modified", models.DateTimeField(auto_now=True)),
-                ("name", models.CharField(max_length=255, verbose_name="name")),
-                ("description", models.TextField(blank=True, verbose_name="description")),
+                (
+                    "id",
+                    models.UUIDField(
+                        default=uuid.uuid4,
+                        editable=False,
+                        primary_key=True,
+                        serialize=False,
+                    ),
+                ),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+                (
+                    "name",
+                    models.CharField(max_length=255, unique=True, verbose_name="name"),
+                ),
+                (
+                    "description",
+                    models.TextField(blank=True, verbose_name="description"),
+                ),
             ],
             options={
                 "verbose_name": "Жанр",
@@ -61,9 +104,17 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name="Person",
             fields=[
-                ("id", models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
-                ("created", models.DateTimeField(auto_now_add=True)),
-                ("modified", models.DateTimeField(auto_now=True)),
+                (
+                    "id",
+                    models.UUIDField(
+                        default=uuid.uuid4,
+                        editable=False,
+                        primary_key=True,
+                        serialize=False,
+                    ),
+                ),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
                 ("full_name", models.TextField(verbose_name="full_name")),
             ],
             options={
@@ -75,11 +126,43 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name="PersonFilmwork",
             fields=[
-                ("id", models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
-                ("role", models.TextField(verbose_name="role")),
-                ("created", models.DateTimeField(auto_now_add=True)),
-                ("film_work_id", models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to="movies.filmwork")),
-                ("person_id", models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to="movies.person")),
+                (
+                    "id",
+                    models.UUIDField(
+                        default=uuid.uuid4,
+                        editable=False,
+                        primary_key=True,
+                        serialize=False,
+                    ),
+                ),
+                (
+                    "role",
+                    models.TextField(
+                        choices=[
+                            ("director", "Director"),
+                            ("actor", "Actor"),
+                            ("writer", "Writer"),
+                        ],
+                        verbose_name="role",
+                    ),
+                ),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                (
+                    "film_work_id",
+                    models.ForeignKey(
+                        db_column="film_work_id",
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="movies.filmwork",
+                    ),
+                ),
+                (
+                    "person_id",
+                    models.ForeignKey(
+                        db_column="person_id",
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="movies.person",
+                    ),
+                ),
             ],
             options={
                 "verbose_name": "Роль персоны",
@@ -90,15 +173,39 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="person",
             name="film_works",
-            field=models.ManyToManyField(through="movies.PersonFilmwork", to="movies.filmwork"),
+            field=models.ManyToManyField(
+                through="movies.PersonFilmwork", to="movies.filmwork"
+            ),
         ),
         migrations.CreateModel(
             name="GenreFilmwork",
             fields=[
-                ("id", models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
-                ("created", models.DateTimeField(auto_now_add=True)),
-                ("film_work_id", models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to="movies.filmwork")),
-                ("genre_id", models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to="movies.genre")),
+                (
+                    "id",
+                    models.UUIDField(
+                        default=uuid.uuid4,
+                        editable=False,
+                        primary_key=True,
+                        serialize=False,
+                    ),
+                ),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                (
+                    "film_work_id",
+                    models.ForeignKey(
+                        db_column="film_work_id",
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="movies.filmwork",
+                    ),
+                ),
+                (
+                    "genre_id",
+                    models.ForeignKey(
+                        db_column="genre_id",
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="movies.genre",
+                    ),
+                ),
             ],
             options={
                 "verbose_name": "Жанр фильма",
@@ -109,24 +216,27 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="filmwork",
             name="genres",
-            field=models.ManyToManyField(through="movies.GenreFilmwork", to="movies.genre"),
-        ),
-        migrations.RunSQL(
-            sql="CREATE INDEX IF NOT EXISTS film_work_creation_date_idx ON content.film_work(creation_date);",
-            reverse_sql="DROP INDEX IF EXISTS film_work_creation_date_idx;",
-        ),
-        migrations.RunSQL(
-            sql=(
-                "CREATE UNIQUE INDEX IF NOT EXISTS film_work_person_idx ON"
-                " content.person_film_work(film_work_id, person_id, role);"
+            field=models.ManyToManyField(
+                through="movies.GenreFilmwork", to="movies.genre"
             ),
-            reverse_sql="DROP INDEX IF EXISTS film_work_person_idx;",
         ),
-        migrations.RunSQL(
-            sql=(
-                "CREATE UNIQUE INDEX IF NOT EXISTS film_work_genre_idx ON "
-                "content.genre_film_work(film_work_id, genre_id);"
+        migrations.AddConstraint(
+            model_name="personfilmwork",
+            constraint=models.UniqueConstraint(
+                fields=("film_work_id", "person_id", "role"),
+                name="film_work_person_idx",
             ),
-            reverse_sql="DROP INDEX IF EXISTS film_work_genre_idx;",
+        ),
+        migrations.AddConstraint(
+            model_name="genrefilmwork",
+            constraint=models.UniqueConstraint(
+                fields=("film_work_id", "genre_id"), name="film_work_genre_idx"
+            ),
+        ),
+        migrations.AddIndex(
+            model_name="filmwork",
+            index=models.Index(
+                fields=["creation_date"], name="film_work_creation_date_idx"
+            ),
         ),
     ]
